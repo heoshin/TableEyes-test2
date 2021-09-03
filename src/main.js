3dcd38b9508e2d64e6b8fc745ae7edb1691cd2f3
허신 <ghtls050@gmail.com>
2021-09-03 18:12:57 +0900

    vuex 추가

--- src/main.js
+++ src/main.js
@@ -2,13 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
 import axios from "axios";
-// import VueAxios from 'vue-axios';
 
 const app = createApp(App);
 
 app.config.globalProperties.axios = axios;
-// app.config.globalProperties.VueAxios = VueAxios;
-
 axios.defaults.withCredentials = true;
 
-app.use(router).mount('#app')
+import store from './store';
+
+app.use(router).use(store).mount('#app')
